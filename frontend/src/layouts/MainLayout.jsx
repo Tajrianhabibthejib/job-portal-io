ba0bb79bbadaceae1d43685f72334b06d79fbec5
Tajrian Habib Thejib <tajrianhabibthejib@gmail.com>
2025-01-11 11:48:25 +0600

    .

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-
+import { ToastContainer } from "react-toastify";
 const MainLayout = () => {
   return (
     <>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Navbar />
       <Outlet />
     </>
